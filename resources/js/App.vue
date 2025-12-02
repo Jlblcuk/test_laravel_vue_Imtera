0df34d50863d5de71345db5d125ae87227b7e740
Jlblcuk <NemerovVV@mail.ru>
2025-12-02 11:45:25 +0500

    Стартовый набор Laravel+Vue

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container mx-auto p-4">
+        <h1 class="text-3xl font-bold underline">
+            Hello Vue + Laravel!
+        </h1>
+    </div>
+</template>
+
+<script setup>
+</script>
